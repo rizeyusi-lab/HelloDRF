14d10184d08b002aa67832cd987ec029b24d2592
na <rizeyusi@gmail.com>
2026-05-22 18:42:14 +0900

    Add files via upload

---
+++ example/urls.py
@@ -0,0 +1,13 @@
+#example/urls.py
+from django.urls import path, include
+from .views import  BookAPIMixins, BooksAPIMixins, HelloAPI, bookAPI, booksAPI, BooksAPI, BookAPI
+
+urlpatterns = [
+    path("hello/", HelloAPI),
+    path("fbv/books/", booksAPI),
+    path("fbv/book/<int:bid>/", bookAPI),
+    path("cbv/books/", BooksAPI.as_view()),
+    path("cbv/book/<int:bid>/", BookAPI.as_view()),
+    path("mixin/books/", BooksAPIMixins.as_view()),
+    path("mixin/book/<int:bid>/", BookAPIMixins.as_view()),
+]
